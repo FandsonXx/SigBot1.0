70455d6d12e05789aadce855a463a58d72d5b4f8
fandson <fandson.morais@gmail.com>
2023-02-03 19:34:54 -0300

    refatorando o codigo

---
+++ app/key.py
@@ -0,0 +1,5 @@
+TELEGRAM_TOKEN = '5639397237:AAHNbwQbvI5-ODj3EZ_zRRI5yGM-Q0_zckY'
+API_URL = f'https://api.telegram.org/bot{TELEGRAM_TOKEN}' + '/{method_name}' + '?chat_id={chat_id}'
+senha = None
+identificador = None
+login = None
